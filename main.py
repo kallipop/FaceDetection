b965ec3ee406be2da1416ba2be97c576a73b1b1a
kallipop <kalliepas05@gmail.com>
2026-09-25 21:03:06 +0300

    Add face counter, status, and screenshot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 camera = cv2.VideoCapture(0)
 
-if not camera.isOpened:
+if not camera.isOpened():
     print("Cannot open camera")
     exit()
     
@@ -21,12 +21,22 @@ while True:
     face = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=8)
     for(x,y,w,h) in face:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(127,0,255),2)
-        
+    
+    cv2.putText(frame,f"Faces detected: {len(face)}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,1,(127,0,255),2)
+    
+    if len(face) >0:
+           cv2.putText(frame,"Status: Face detected",(20,100),cv2.FONT_HERSHEY_SIMPLEX,1,(127,0,255),2)
+    else:
+        cv2.putText(frame,"Status: No Face detected",(20,100),cv2.FONT_HERSHEY_SIMPLEX,1,(127,0,255),2)
     
     cv2.imshow("My camera", frame)
     
-    if cv2.waitKey(1) == ord('q'):
+    key = cv2.waitKey(1)
+    
+    if key == ord('q'):
         break
+    elif key == ord('s'):
+        cv2.imwrite("screenshot.png",frame)
     
 camera.release()
 cv2.destroyAllWindows()
